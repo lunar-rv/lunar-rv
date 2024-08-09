Fix: epsilon value added to prevent zero division in entropy calculation

graphs.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import argparse
 import logging
 from datetime import datetime, timedelta
 import json
@@ -38,35 +37,15 @@ def plot_array(trace: np.ndarray, sensor_index: int, batch_start_time: datetime,
     units = "mBar" if sensor_type == "PRESSURE" else "Kelvin"
     plt.ylabel(f"Sensor {sensor_index+1} {keyword} ({units})")
     if boundary is not None:
-        plt.axhline(y=boundary, color="red", linestyle="--")
-    if backlog_size == 0:
-        date_str = f"on {batch_start_time.strftime('%Y/%m/%d')}"
+        plt.axhline(y=boundary, color="red", linestyle="--", label="Formula boundary")
+    if trace_start_time.day == batch_start_time.day:
+        date_str = f"on {trace_start_time.strftime('%Y/%m/%d')}"
     else:
         date_str = f"from {trace_start_time.strftime('%Y/%m/%d')} to {batch_start_time.strftime('%Y/%m/%d')}"
     plt.title(f"{keyword} for {sensor_type.capitalize()} Sensor {sensor_index+1} {date_str}")
+    if boundary is not None:
+        rob_metric = "min" if config["USE_MIN"] else "mean" 
+        plt.text(0.95, 0.01, f"Robustness metric: {rob_metric}", transform=plt.gca().transAxes,
+         fontsize=8, verticalalignment='bottom', horizontalalignment='right')
     plt.legend()
     plt.show()
-
-def main():
-    parser = argparse.ArgumentParser(description="Plot traces")
-    parser.add_argument(
-        "-p",
-        "--pos_infile",
-        default="csv/positive_test.csv",
-        help="Positives input file",
-    )
-    parser.add_argument(
-        "-n",
-        "--neg_infile",
-        default="csv/negative_test.csv",
-        help="Negatives input file",
-    )
-    parser.add_argument(
-        "-o", "--outfile", default="images/residuals.png", help="Output file for graph"
-    )
-    args = parser.parse_args()
-    plot_traces(args.neg_infile, args.pos_infile, args.outfile)
-
-
-if __name__ == "__main__":
-    main()

model.py
@@ -36,66 +36,68 @@ def get_residuals(
         abs_residuals *= 1000 # mBar
     return abs_residuals
 
-def get_safety_prob(sensor_index, mean_rob: float, sensor_type=None) -> float:
+def get_safety_dist(sensor_index, sensor_type=None) -> float:
     safe_residuals_file = get_filename("residuals", sensor_index, sensor_type=sensor_type)
     safe_traces = np.genfromtxt(safe_residuals_file, delimiter=",", dtype=float)
     sigma = np.std(safe_traces)
     mu = np.mean(safe_traces)
-    return stats.norm.cdf(mean_rob, mu, sigma)
+    return mu, sigma
 
 def new_batch_ok(residuals, formula=None, new_batch: list = None, sensor_index: int = None, sensor_type: str = None) -> bool:
     if formula:
         raw_data = preprocess("".join(new_batch), csv=False)
         sensor_values = raw_data[:, sensor_index]
+        backlog_size: int = formula.last_residuals.size if formula.last_residuals is not None else 0
+        if backlog_size != 0: # it was None, now it isn't
+            old_residuals = np.hstack((formula.last_residuals.flatten(), residuals))
+            old_sensor_values = np.hstack((formula.last_raw_values.flatten(), sensor_values))
+        else:
+            old_residuals = residuals
+            old_sensor_values = sensor_values
         evaluation = formula.evaluate_single(residuals, labels=False, raw_values=sensor_values)
-        ### NEED TO DEAL WITH PRINTING ANOMALY INFO, BUT DO THIS LATER
-        # safety_prob = get_safety_prob(sensor_index=sensor_index, mean_rob=classification, sensor_type=sensor_type)
-        # rounded_rob = np.round(classification, 4)
-        # print("Robustness: ", rounded_rob)
-        # print(f"Likelihood of robustness {rounded_rob} or lower: {np.round(safety_prob, 4)}")
-        # print(f"Minimum threshold: {np.round(get_safety_prob(sensor_index=sensor_index, mean_rob=0, sensor_type=sensor_type), 4)}")
-        if evaluation.min() < 0:
-            anomaly_start_indices = np.where(evaluation < 0)[0].tolist()
-            bounds, batch_start_time = get_and_display_anomaly_times(anomaly_start_indices, formula, new_batch)
-            if formula.last_residuals is not None:
-                bounds = np.array(bounds) + 2 * formula.last_residuals.size
-                residuals = np.hstack((formula.last_residuals.flatten(), residuals))
-                sensor_values = np.hstack((formula.last_raw_values.flatten(), sensor_values))
-            #### ADD formula.last_residuals TO START OF RESIDUALS ####
-            ### Problem is that I haven't stored the raw sensor values in 'last' ###
-            ### Could extract these values from a file somewhere ###
+        rob = evaluation.min() if config["USE_MIN"] else evaluation.mean()
+        if rob < 0:
+            mean_rob = np.round(evaluation.mean(), 4)
+            min_rob = np.round(evaluation.min(), 4)
+            mu, sigma = get_safety_dist(sensor_index, sensor_type)
+            mean_safety_prob = np.round(stats.norm.cdf(mean_rob, mu, sigma), 4)
+            min_safety_prob = np.round(stats.norm.cdf(min_rob, mu, sigma), 4)
+            print("Average robustness:", mean_rob)
+            print("Minimum robustness:", min_rob)
+            print(f"Likelihood of robustness {mean_rob} or lower: {mean_safety_prob}")
+            print(f"Likelihood of robustness {min_rob} or lower: {min_safety_prob}")
+            if config["USE_MIN"]:
+                anomaly_start_indices = np.where(evaluation < 0)[0].tolist()
+            else:
+                anomaly_start_indices = list(range(len(evaluation)))
+            bounds, batch_start_time = get_and_display_anomaly_times(anomaly_start_indices, formula, new_batch, prev_backlog_size=backlog_size)
+            bounds = [] if not config["USE_MIN"] else np.array(bounds) + backlog_size
             if config["PLOT_ANOMALY_GRAPHS"]:
-                # print(anomaly_start_indices)
-                # print(residuals.tolist())
-                # exit()
                 plot_array(
-                    trace=sensor_values,
+                    trace=old_sensor_values,
                     sensor_index=sensor_index,
                     keyword="Actual Sensor Values",
                     bounds=bounds,
                     sensor_type=sensor_type,
                     batch_start_time=batch_start_time,
-                    backlog_size=formula.last_residuals.size,
+                    backlog_size=backlog_size,
                 )
                 plot_array(
-                    trace=residuals, 
+                    trace=old_residuals, 
                     sensor_index=sensor_index, 
                     keyword="Magnitude of Residuals", 
                     boundary=formula.boundary, 
                     bounds=bounds,
                     batch_start_time=batch_start_time,
-                    backlog_size=formula.last_residuals.size,
+                    backlog_size=backlog_size,
                     sensor_type=sensor_type,
                 )
-
-            # print_anomaly_info(model, new_batch, formula, sensor_type)
+            print_anomaly_info(model, new_batch, formula, sensor_type)
             return False
     return True
 
 def update_spec(
     sensor_index,
-    operators=config["OPERATORS"],
-    invariance=config["INVARIANCE"],
     bin_classifier=None,
     new_trace=None,
     new_label=None,
@@ -107,15 +109,16 @@ def update_spec(
     anomalies_file = get_filename("anomalies", sensor_index, sensor_type=sensor_type)
     negative_traces = np.genfromtxt(residuals_file, delimiter=",", dtype=float)
     positive_traces = np.genfromtxt(anomalies_file, delimiter=",")
-    use_mean = config["USE_MEAN"]
+
     if len(positive_traces) < config["WARMUP_ANOMALIES"] or positive_traces.ndim == 1:
-        spec = positive_synth(operator="F", traces=negative_traces)
+        prev_formula = formulae[sensor_index] if len(formulae) > sensor_index else None
+        spec = positive_synth(operator="F", traces=negative_traces, prev_formula=prev_formula)
     elif len(positive_traces) == config["WARMUP_ANOMALIES"]:
         positive_values = positive_traces[:, :-1].astype(float)
-        bin_classifier = build(negative_traces, positive_values, invariance=invariance, use_mean=use_mean)
+        bin_classifier = build(negative_traces, positive_values)
         spec = bin_classifier.formula
     else:
-        bin_classifier = update(bin_classifier, new_trace, new_label, invariance=invariance, use_mean=use_mean)
+        bin_classifier = update(bin_classifier, new_trace, new_label)
         spec = bin_classifier.formula
     formulae[sensor_index] = spec
     if not os.path.exists(spec_file):
@@ -137,9 +140,6 @@ def update_spec(
 def log_anomaly(
     batch, trace, sensor_index, tree=None, warmup2=False, sensor_type=None
 ) -> TreeNode:
-    raw_data = preprocess(
-        "".join(batch), csv=False, time_features=False, season_features=False
-    )
     trace_np = np.array(trace.split(",")).astype(float)
     if tree:
         prediction = tree.classify(trace_np)

synth.py
@@ -37,11 +37,11 @@ def weighted_closeness(arr: np.ndarray, interval: int, trace_length=-1) -> float
     gamma = config["GAMMA"]
     contraction_fn = lambda r, size: r * (1 + np.exp((size - 0.5) * gamma))
     score = -arr.ptp() # Aim to maximise score
-    proportion = np.log(interval) / np.log(trace_length) # Log may not be helpful actually
+    proportion = interval / trace_length # Log may not be helpful actually
     result = contraction_fn(score, proportion)
     return result
     
-def positive_synth(traces, operator="F"):
+def positive_synth(traces, operator="F", prev_formula=None):
     trace_length = traces.size // traces.shape[0]
     best_score = -np.inf
     best_formula = None
@@ -49,11 +49,23 @@ def positive_synth(traces, operator="F"):
         for i in range(1, trace_length):
             formula = Formula.build_formula(0, "F", i, "<=")
             evaluation = formula.evaluate3(traces, labels=False).min(axis=1)
-            threshold = evaluation.min()
+            threshold = evaluation.min() if config["USE_MIN"] else evaluation.mean()
             score = weighted_closeness(evaluation, interval=i, trace_length=trace_length)
             if score > best_score:
                 best_score = score
                 best_formula = Formula.build_formula(-threshold, "F", i, "<=")
+        if prev_formula:
+            lrv = prev_formula.last_raw_values
+            lr = prev_formula.last_residuals
+            if prev_formula.end > best_formula.end:
+                lrv = lrv[1-best_formula.end:]
+                lr = lr[1-best_formula.end:]
+            elif prev_formula.end < best_formula.end:
+                padding_length = best_formula.end - prev_formula.end
+                lrv = np.pad(lrv, ((0, 0), (padding_length, 0)), mode='constant', constant_values=np.nan)
+                lr = np.pad(lr, ((0, 0), (padding_length, 0)), mode='constant', constant_values=np.nan)
+            best_formula.last_raw_values = lrv
+            best_formula.last_residuals = lr
     elif operator == "G":
         best_formula = Formula.build_formula(traces.max(), "G", "<=")
     return best_formula

tree/bin_class.py
@@ -4,7 +4,7 @@ from tree.tree import TreeNode
 from sklearn.model_selection import train_test_split
 
 
-def build(neg_train, pos_train, invariance=False, use_mean=True):
+def build(neg_train, pos_train):
     neg_label_train = np.full((neg_train.shape[0], 1), "Safe")
     pos_label_train = np.full((pos_train.shape[0], 1), "Anomaly")
     train_data = np.vstack(
@@ -13,14 +13,14 @@ def build(neg_train, pos_train, invariance=False, use_mean=True):
             np.hstack((pos_train, pos_label_train)),
         )
     )
-    head = TreeNode.build_tree(train_data, max_depth=1, binary=True, invariance=invariance, use_mean=use_mean)
+    head = TreeNode.build_tree(train_data, max_depth=1, binary=True)
     return head
 
 
-def update(tree, new_values, label, invariance=False, use_mean=True):
+def update(tree, new_values, label):
     label = np.array([label])
     new_trace = np.hstack((new_values, label))
-    tree.update_tree(new_trace, binary=True, invariance=invariance, use_mean=use_mean)
+    tree.update_tree(new_trace, binary=True)
     return tree
 
 
@@ -38,8 +38,8 @@ def main():
     formula = build(neg_train=neg_train, pos_train=pos_train).formula
 
     # Evaluate the model on the test set
-    neg_predictions = formula.evaluate(neg_test)
-    pos_predictions = formula.evaluate(pos_test)
+    neg_predictions = formula.evaluate3(neg_test).min(axis=1)
+    pos_predictions = formula.evaluate3(pos_test).min(axis=1)
     # Combine classifications and ground truths
     print(neg_predictions.shape)
     predictions = np.vstack((neg_predictions, pos_predictions)).flatten()

tree/formula.py
@@ -24,46 +24,30 @@ class Formula:
         return self.spec
 
     @staticmethod
-    def build_formula(boundary: float, operator="_", end: int = -1, sign="<="):
+    def build_formula(boundary: float, operator="_", end: int = -1, sign="<=", last_residuals=None, last_raw_values=None):
         if operator == "_":
             return Basic(boundary, end=end, sign=sign)
         elif operator == "F":
-            return Eventually(boundary, end=end, sign=sign)
+            return Eventually(boundary, end=end, sign=sign, last_residuals=last_residuals, last_raw_values=last_raw_values)
         elif operator == "G":
             return Always(boundary, end=end, sign=sign)
         else:
             raise ValueError(f"Unknown operator: {operator}")
 
     @staticmethod
-    def list_options(operators="FG_", boundaries=None, binary=True, invariance=False, use_mean=True):
-        if invariance:
-            if use_mean:
-                return [(b, 1, "G") for b in boundaries]
-            else:
-                return [(b, config["BATCH_SIZE"], "G") for b in boundaries]
+    def list_options(boundaries=[], binary=True):
         all_options = []
-        time_period = config["BATCH_SIZE"]
         def add_options(end, op, b=None):
             if binary:
                 all_options.append((0, end, op))
             else:
                 all_options.append((b, end, op))
-        for op in operators:
-            if op in "FG":
-                end = 1
-                while time_period % end == 0 and time_period > end:
-                    if binary:
-                        add_options(end, op)
-                    else:
-                        for b in boundaries:
-                            add_options(end, op, b)
-                    end *= 2
+        for end in range(1, config["BATCH_SIZE"]):
+            if binary:
+                add_options(end, "F")
             else:
-                if binary:
-                    add_options(1, op)
-                else:
-                    for b in boundaries:
-                        add_options(1, op, b)
+                for b in boundaries:
+                    add_options(end, "F", b)
         return all_options
 
     @abstractmethod
@@ -135,10 +119,10 @@ class BoundedFormula(Formula, ABC):
         return all_values.mean(axis=1)
     
 class Eventually(BoundedFormula):
-    def __init__(self, boundary, end: int, sign=">="):
+    def __init__(self, boundary, end: int, sign=">=", last_residuals=None, last_raw_values=None):
         super().__init__(boundary, sign, end)
-        self.last_residuals = None
-        self.last_raw_values = None
+        self.last_residuals = last_residuals
+        self.last_raw_values = last_raw_values
     @property
     def spec(self):
         return f"eventually[0:{self.end})(error {self.sign} {self.boundary})"
@@ -163,21 +147,18 @@ class Eventually(BoundedFormula):
         return all_values
     
     def evaluate_single(self, trace, raw_values: np.ndarray, labels=True):
-        print(raw_values.shape)
         raw_values = raw_values.reshape(1, -1)
         traces_arr = trace.reshape(1, -1)
         traces_arr = np.hstack((self.last_residuals, traces_arr)) if self.last_residuals is not None else traces_arr
         raw_values = np.hstack((self.last_raw_values, raw_values)) if self.last_raw_values is not None else raw_values
-        evaluation = self.evaluate3(traces=traces_arr, labels=labels)
+        evaluation = self.evaluate3(traces=traces_arr, labels=labels)[0]
         if self.last_residuals is None or self.end <= traces_arr.shape[1]:
-            if self.last_residuals is not None:
-                print("LRV shape:", self.last_raw_values.shape)
             self.last_residuals = traces_arr[:, -self.end + 1:]
             self.last_raw_values = raw_values[:, -self.end + 1:]
         else:
             self.last_residuals = traces_arr
             self.last_raw_values = raw_values
-        return evaluation[0]
+        return evaluation
     
     def human_readable(self):
         frequency = 60 // config["TIME_PERIOD"]
@@ -321,10 +302,15 @@ def get_anomaly_bounds(indices) -> list:
     return bounds
 
 if __name__ == "__main__":
-    formula = Formula.build_formula(0.03389871128318955, "F", 22, "<=")
-    trace = np.array([0.0425063702280612, 0.18826705324576434, 0.4012793802426924, 0.524821307393307, 0.4012793802426924, 0.2777362156046216, 0.08972943046809193, 0.2187346138168532, 0.08972943046809193, 0.03927549130232355, 0.07830282062214547, 0.20914935263380632, 0.07830282062214547, 0.05254557409208396, 0.2529852132780244, 0.2985415138590797, 0.2529852132780244, 0.20742634423103617, 0.04664647645037656, 0.18886567125377465, 0.04664647645037656, 0.08322846402686324, 0.026415579590776533, 0.04456207795779099, 0.026415579590776533, 0.097391329790468, 0.14059696562890647, 0.056135059450304614, 0.14059696562890647, 0.22505910966661574, 0.034562718659123665, 0.07952086098149402, 0.034562718659123665, 0.010397645803916084, 0.08506007625892764, 0.19154417566016185, 0.08506007625892764, 0.021425930491116713, 0.05393736400750859, 0.2951377141758929, 0.05393736400750859, 0.1872615554605328, 0.1230854419233332, 0.132138942095468, 0.1230854419233332, 0.11403031783788556, 0.05873009340146701, 0.025932757779956184, 0.05873009340146701, 0.14339103723418067, 0.08308112943081203, 0.07810169207243464, 0.08308112943081203, 0.08806014353105254, 0.07118642531960331, 0.10066471416741996, 0.07118642531960331, 0.04170941405984632, 0.17670088049139693, 0.21716866414733804, 0.17670088049139693, 0.13623405963153132, 0.010912479897609573, 0.12116227758785653, 0.010912479897609573, 0.09933414142337949, 0.07886698900990904, 0.1203811029106272, 0.07886698900990904, 0.0373523756720083, 0.19994758761238476, 0.18341525718406845, 0.19994758761238476, 0.21648105580612462, 0.00836433863940153, 0.07948559696224419, 0.00836433863940153, 0.06275466600965535, 0.047952120222787, 0.03685606157304358, 0.047952120222787, 0.05904706296424797, 0.08613621388102455, 0.13012446222874022, 0.08613621388102455, 0.042148118645540106, 0.2290945076316095, 0.23088366088390086, 0.2290945076316095, 0.2273081929909833, 0.22096992196038445, 0.1937335330804632, 0.22096992196038445, 0.2482059645152017, 0.0425063702280612, 0.1032554733440133, 0.0425063702280612, 0.18826705324576434, 0.4012793802426924, 0.524821307393307, 0.4012793802426924, 0.2777362156046216, 0.08972943046809193, 0.2187346138168532, 0.08972943046809193, 0.03927549130232355, 0.07830282062214547, 0.20914935263380632, 0.07830282062214547, 0.05254557409208396, 0.2529852132780244, 0.2985415138590797, 0.2529852132780244, 0.20742634423103617, 0.04664647645037656, 0.18886567125377465, 0.04664647645037656])
-    evals = formula.evaluate_single(trace)
-    anomaly_start_indices = np.where(evals < 0)[0].tolist()
+    boundary = 0.025018738211056435
+    formula = Formula.build_formula(boundary, "F", 24, "<=")
+    trace = np.array([0.28547559247962817, 0.26462692391961834, 0.28547559247962817, 0.3063249211704179, 0.008126966558354848, 0.052574843004894734, 0.008126966558354848, 0.06882811599092514, 0.0031756988694703336, 0.08547719184467603, 0.0031756988694703336, 0.07912368403458978, 0.20283734279601118, 0.2059510238821248, 0.20283734279601118, 0.19972366170989755, 0.29949484326185705, 0.336089247907366, 0.29949484326185705, 0.2629003874166716, 0.4290392382291681, 0.5070291539974736, 0.4290392382291681, 0.3089858367679409, 0.14197785774081734, 0.09101568911105973, 0.14197785774081734, 0.192942402568147, 0.07308060791669929, 0.19059606368985668, 0.07308060791669929, 0.04443304068794257, 0.14604731202853827, 0.2007189769743338, 0.14604731202853827, 0.09137810532240664, 0.15927335689061572, 0.017395970448575576, 0.15927335689061572, 0.3011521549248844, 0.24963361718947918, 0.13444613551068588, 0.24963361718947918, 0.3648229060367117, 0.6427681625987315, 0.46247514145985813, 0.6427681625987315, 0.8230630910864046, 0.4092410020264184, 0.41902519699403906, 0.4092410020264184, 0.39945748503763046, 0.1133058573260376, 0.14649739708083934, 0.1133058573260376, 0.0801145267012339, 0.156290267218865, 0.013706081577545548, 0.156290267218865, 0.29887254551135695, 0.01189490346697672, 0.13108053474642986, 0.01189490346697672, 0.15486901213030216, 0.020105034374811454, 0.07203256631765562, 0.020105034374811454, 0.11224255302530461, 0.19832857972537643, 0.1232583367795359, 0.19832857972537643])
+    evals = formula.evaluate3(trace.reshape(1, -1))
+    anomaly_start_indices = np.where(evals[0] < 0)[0].tolist()
+    print(anomaly_start_indices)
+    print("Success points:", np.where(trace < boundary)[0].tolist())
+
+    exit()
     bounds = get_anomaly_bounds(anomaly_start_indices)
     print(bounds)
     # for trace in traces:

tree/tree.py
@@ -6,7 +6,8 @@ import random
 from ui import print_score # for testing purposes only
 import json
 with open("config.json") as file:
-    tree_config = json.load(file)["TREE_CONFIG"]
+    config = json.load(file)
+tree_config = config["TREE_CONFIG"]
 
 def entropy(left_lab, right_lab) -> float:
     def calculate_entropy(labels):
@@ -27,26 +28,26 @@ def entropy(left_lab, right_lab) -> float:
     total_entropy = left_weight * left_entropy + right_weight * right_entropy
     return total_entropy
 
-
 def stl_entropy(left_lab, left_rob, right_lab, right_rob) -> float:
+    total_size = len(left_rob) + len(right_rob)
+    l = len(left_rob) / total_size
+    r = len(right_rob) / total_size
+
     def calculate_entropy(lab, rob):
-        size = len(lab)
-        if size == 0:
+        if lab.size == 0:
             return 0
         abs_rob = np.abs(rob)
         classes = np.unique(lab)
         classified_rob_sums = dict.fromkeys(classes, 0)
-        for i in range(size):
+        for i in range(lab.size):
             l = lab[i]
+            if abs_rob[i] == 0:
+                abs_rob[i] = 1e-6 # To avoid division by zero
             classified_rob_sums[l] += abs_rob[i]
         rob_sums = list(classified_rob_sums.values())
         probabilities = rob_sums / np.sum(rob_sums)
         entropy_value = -np.sum(probabilities * np.log(probabilities))
         return entropy_value
-
-    total_size = len(left_rob) + len(right_rob)
-    l = len(left_rob) / total_size
-    r = len(right_rob) / total_size
 
     H_left = calculate_entropy(left_lab, left_rob)
     H_right = calculate_entropy(right_lab, right_rob)
@@ -77,7 +78,7 @@ def find_best_binary_threshold(values, labels, n=20) -> float:
 
 
 def split_with_formula(traces: np.ndarray, formula: Formula, return_traces=False, binary=True) -> tuple:
-    evaluations = formula.evaluate(traces)
+    evaluations = formula.evaluate3(traces).min(axis=1)
     if return_traces:
         left_trace = traces[evaluations > 0]
         right_trace = traces[evaluations <= 0]
@@ -86,7 +87,7 @@ def split_with_formula(traces: np.ndarray, formula: Formula, return_traces=False
     if binary:
         threshold = find_best_binary_threshold(evaluations, labels, n=20)
         formula.boundary = -threshold
-        evaluations = formula.evaluate(traces)
+        evaluations = formula.evaluate3(traces).min(axis=1)
     left_lab = labels[evaluations > 0]
     left_rob = evaluations[evaluations > 0]
     right_lab = labels[evaluations <= 0]
@@ -94,12 +95,12 @@ def split_with_formula(traces: np.ndarray, formula: Formula, return_traces=False
     return left_lab, left_rob, right_lab, right_rob
 
 
-def choose_formula(traces: np.ndarray, binary=False, operators="FG_", invariance=False, use_mean=True) -> Formula:
+def choose_formula(traces: np.ndarray, binary=False) -> Formula:
     best_entropy = np.inf
     best_formula = None
     values = traces[:, :-1].astype(float)
-    boundaries = np.linspace(values.min(), values.max(), num=100)
-    for boundary, end, operator in Formula.list_options(binary=binary, boundaries=boundaries, operators=operators, invariance=invariance, use_mean=use_mean):
+    boundaries = np.linspace(values.min(), values.max(), num=3)
+    for boundary, end, operator in Formula.list_options(binary=binary, boundaries=boundaries):
         formula = Formula.build_formula(boundary=boundary, end=end, operator=operator)
         left_lab, left_rob, right_lab, right_rob = split_with_formula(traces, formula, binary=binary)
         H_1 = stl_entropy(left_lab, left_rob, right_lab, right_rob)
@@ -188,8 +189,9 @@ class TreeNode:
     def classify(self, trace) -> str:
         if self.value:
             return self.value
-        rob = self.formula.evaluate(trace.reshape(1, -1), labels=False)
-        next_node = self.left if rob.mean() > 0 else self.right
+        evaluation = self.formula.evaluate3(trace.reshape(1, -1), labels=False)[0].min()
+        rob = evaluation.min() if config["USE_MIN"] else evaluation.mean()
+        next_node = self.left if rob > 0 else self.right
         return next_node.classify(trace)
 
     def print_tree(self, stem=""):
@@ -204,22 +206,20 @@ class TreeNode:
             self.right.print_tree(stem=r_stem)
 
     @staticmethod
-    def build_tree(traces: np.ndarray, depth=0, max_depth=tree_config["MAX_DEPTH"], binary=False, invariance=False, use_mean=True):
-        if invariance:
-            max_depth = 1
+    def build_tree(traces: np.ndarray, depth=0, max_depth=tree_config["MAX_DEPTH"], binary=False):
         if 0 in traces.shape or traces.ndim == 0:
             return None
         labels = traces[:, -1]
         if len(np.unique(labels)) == 1 or depth == max_depth:
             return TreeNode(None, None, traces, None, value=choose_majority(labels), max_depth=max_depth)
-        formula = choose_formula(traces, binary=binary, invariance=invariance, use_mean=use_mean)
+        formula = choose_formula(traces, binary=binary)
         left_traces, right_traces = split_with_formula(
             traces, formula, return_traces=True, binary=binary
         )
         if len(left_traces) == 0 or len(right_traces) == 0:
             return TreeNode(None, None, traces, None, value=choose_majority(labels), max_depth=max_depth)
-        left_node = TreeNode.build_tree(left_traces, depth=depth+1, max_depth=max_depth, binary=binary, use_mean=use_mean)
-        right_node = TreeNode.build_tree(right_traces, depth=depth+1, max_depth=max_depth, binary=binary, use_mean=use_mean)
+        left_node = TreeNode.build_tree(left_traces, depth=depth+1, max_depth=max_depth, binary=binary)
+        right_node = TreeNode.build_tree(right_traces, depth=depth+1, max_depth=max_depth, binary=binary)
         return TreeNode(left_node, right_node, traces, formula, max_depth=max_depth)
 
     # def update_tree(self, trace: np.ndarray, depth=0, binary=False, invariance=False, use_mean=True) -> None:
@@ -235,10 +235,10 @@ class TreeNode:
     #         next_node = self.left if rob > 0 else self.right
     #         next_node.update_tree(trace, depth=depth+1, binary=binary, invariance=invariance, use_mean=use_mean)
 
-    def update_tree(self, trace: np.ndarray, depth=0, binary=False, invariance=False, use_mean=True) -> None:
+    def update_tree(self, trace: np.ndarray, depth=0, binary=False) -> None:
         def rebuild_tree():
             print("Rebuilding tree...")
-            new_tree = self.build_tree(self.traces, depth=depth, binary=binary, max_depth=self.max_depth, invariance=invariance, use_mean=use_mean)
+            new_tree = self.build_tree(self.traces, depth=depth, binary=binary, max_depth=self.max_depth)
             if new_tree.left or new_tree.right:
                 new_tree.value = None
             self.__dict__.update(new_tree.__dict__)
@@ -253,7 +253,7 @@ class TreeNode:
         if trace[-1] != expected_label:
             rebuild_tree()
         else:
-            next_node.update_tree(trace, depth=depth+1, binary=binary, invariance=invariance, use_mean=use_mean)
+            next_node.update_tree(trace, depth=depth+1, binary=binary)
                  
 
 

ui.py
@@ -178,7 +178,7 @@ def print_anomaly_info(model, new_batch, formula, sensor_type):
     print(f"Actual average was {y.mean() * 1000 if sensor_type == 'PRESSURE' else y.mean()}")
     print(f"STL formula was: {formula}")
 
-def get_and_display_anomaly_times(anomaly_indices: np.ndarray, formula, new_batch: list) -> None:
+def get_and_display_anomaly_times(anomaly_indices: list, formula, new_batch: list, prev_backlog_size: int) -> None:
     time_period = config["TIME_PERIOD"]
     def get_anomaly_bounds(indices) -> list:
         bounds = []
@@ -187,9 +187,9 @@ def get_and_display_anomaly_times(anomaly_indices: np.ndarray, formula, new_batc
         for i in range(N):
             this_value = indices[i]
             if i == 0 or indices[i-1] + 1 != this_value:
-                start_bound = this_value - formula.end + 1
+                start_bound = this_value - prev_backlog_size
             if i+1 == N or indices[i+1] - 1 != this_value:
-                bounds.append((start_bound, this_value))
+                bounds.append((start_bound, this_value + formula.end - prev_backlog_size - 1))
         return bounds
     print("Formula was:", formula)
     print(f"This means: {formula.human_readable()}.")
@@ -201,8 +201,8 @@ def get_and_display_anomaly_times(anomaly_indices: np.ndarray, formula, new_batc
     start_time = datetime.strptime(datetime_str, "%d/%m/%Y %H:%M:%S")
     bounds = get_anomaly_bounds(anomaly_indices)
     for interval in bounds:
-        interval_start = (start_time + timedelta(minutes = interval[0] * time_period)).strftime("%d/%m/%Y %H:%M:%S")
-        interval_end = (start_time + timedelta(minutes = interval[1] * time_period)).strftime("%d/%m/%Y %H:%M:%S")
+        interval_start = (start_time + timedelta(minutes = (interval[0]) * time_period)).strftime("%d/%m/%Y %H:%M:%S")
+        interval_end = (start_time + timedelta(minutes = (interval[1]) * time_period)).strftime("%d/%m/%Y %H:%M:%S")
         print(f"\t{interval_start} to {interval_end}")
     return bounds, start_time
     # times = [str((start_time + timedelta(minutes=i * frequency)).time()) for i in anomaly_indices]
@@ -230,4 +230,4 @@ if __name__ == "__main__":
     formula = Formula.build_formula(0.1, "F", 6, "<=")
     new_batch = ['PDM23;03/01/2023;00:00:00;0.0389000015258789;Pressione a valle\n', 
                  'PDM24;03/01/2023;00:00:00;0.0362999992370605;Pressione a valle\n']
-    print_anomaly_times(anomaly_indices=anomaly_indices, new_batch=new_batch, formula=formula)
+    get_and_display_anomaly_times(anomaly_indices=anomaly_indices, new_batch=new_batch, formula=formula)