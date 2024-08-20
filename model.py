Fix: classification tree now uses correct formula type

model.py
@@ -57,7 +57,7 @@ def new_batch_ok(residuals, formula=None, new_batch: list = None, sensor_index: 
         evaluation, separated_evals = formula.evaluate_single(residuals, labels=False, raw_values=sensor_values, return_2d=True)
         rob = evaluation.min()
         if rob < 0:
-            print("Evaluation", evaluation)
+            # print("Evaluation", evaluation)
             print("Failed to satisfy formula: ", formula)
             mean_rob = np.round(evaluation.mean(), 4)
             min_rob = np.round(evaluation.min(), 4)
@@ -72,21 +72,13 @@ def new_batch_ok(residuals, formula=None, new_batch: list = None, sensor_index: 
             if config["PLOT_ANOMALY_GRAPHS"]:
                 for i in range(3):
                     phi = formula[i]
-                    this_evaluation = separated_evals[i][:shortest_length]
+                    this_evaluation = separated_evals[i]#[:shortest_length]
                     if this_evaluation.min() >= 0:
                         continue
-                    print("i", phi.__class__, this_evaluation)
-                    print("phi:", phi)
-                    print("eval", len(evaluation))
-                    print("Length of this evaluation:", len(this_evaluation))
-                    print("Last residuals:", old_residuals)
-                    print("New residuals:", residuals)
                     anomaly_start_indices = np.where(this_evaluation < 0)[0].tolist()
-                    print("Anomaly indices:", anomaly_start_indices)
                     end = phi.end if phi.end is not None else formula.max_length
                     bounds, batch_start_time = get_and_display_anomaly_times(anomaly_start_indices, formula, new_batch, prev_backlog_size=backlog_size, end=end)
                     bounds = np.array(bounds) + backlog_size
-                    print("Bounds", bounds)
                     plot_array(
                         trace=old_sensor_values,
                         sensor_index=sensor_index,

synth.py
@@ -1,6 +1,6 @@
 print("Loading formula synthesis functions...")
 from searching import hill_climbing_search
-from tree.new_formula import Formula
+from tree.new_formula import FormulaFactory
 import numpy as np
 import json
 with open("config.json") as config_file:
@@ -10,7 +10,7 @@ with open("config.json") as config_file:
 contraction_fn = lambda r, b, max_size: r * np.exp((0.5 * b / max_size)-0.5)
 
 def evaluate_formula(traces, F_end, G_avg_end):
-    formula = Formula.build_formula(
+    formula = FormulaFactory.build_tightest_formula(
         traces=traces,
         F_end=F_end,
         G_avg_end=G_avg_end,
@@ -26,7 +26,7 @@ def positive_synth(traces, prev_formula=None, reading_type="PRESSURE"):
     best_formula = None
     batch_size = traces.size // traces.shape[0]
     best_x, best_y, _ = hill_climbing_search(traces, batch_size, evaluate_formula)
-    best_formula = Formula.build_formula(
+    best_formula = FormulaFactory.build_tightest_formula(
         traces=traces,
         F_end=best_x,
         G_avg_end=best_y,

tree/formula.py
@@ -131,6 +131,7 @@ class Eventually(BoundedFormula):
         split_traces = traces.reshape(traces.shape[0], -1, self.end)
         values = self.boundary - np.min(split_traces, axis=2)
         return values
+
     
     def evaluate3(self, traces, labels=True): # For testing purposes
         traces = traces[:, :-1].astype(float) if labels else traces
@@ -146,6 +147,24 @@ class Eventually(BoundedFormula):
                 all_values = np.hstack((all_values, values))
         return all_values
     
+    def evaluate4(self, traces, labels=True, beta=10000): # Added beta as a parameter
+        def log_min_approx(x, beta=10000):
+            return - (1/beta) * np.log(np.sum(np.exp(-beta * x), axis=1))
+        traces = traces[:, :-1].astype(float) if labels else traces
+        end = self.end
+        trace_end = traces.shape[1]
+        all_values = [None]
+        for start in range(trace_end - end + 1):
+            cut_traces = traces[:, start:start+end]
+            values = self.boundary - log_min_approx(cut_traces, beta).reshape(-1, 1)
+            
+            if start == 0:
+                all_values = values
+            else:
+                all_values = np.hstack((all_values, values))
+        
+        return all_values
+
     def evaluate_single(self, trace, raw_values: np.ndarray, labels=True):
         raw_values = raw_values.reshape(1, -1)
         traces_arr = trace.reshape(1, -1)
@@ -251,7 +270,7 @@ def get_score(formula, residuals, i=-1):
     score = formula.evaluate3(residuals, labels=False)
     proportion = np.log(i) / np.log(96)
     score = -score.std()
-    # score = contraction_fn(score, proportion)
+    score = contraction_fn(score, proportion)
     return score
 
 def plot_residuals():

tree/tree.py
@@ -1,6 +1,6 @@
 print("Loading code for decision tree construction...")
 import numpy as np
-from tree.formula import Formula
+from tree.new_formula import FormulaFactory
 from tree.compare import compare
 import random
 from ui import print_score # for testing purposes only
@@ -77,8 +77,13 @@ def find_best_binary_threshold(values, labels, n=20) -> float:
     return best_threshold
 
 
-def split_with_formula(traces: np.ndarray, formula: Formula, return_traces=False, binary=True) -> tuple:
-    evaluations = formula.evaluate3(traces).min(axis=1)
+def split_with_formula(traces: np.ndarray, formula, return_traces=False, binary=True) -> tuple:
+    try:
+        evaluations = formula.evaluate(traces, labels=True).min(axis=1)
+    except:
+        print("Error evaluating formula", formula)
+        print("Traces:", traces)
+        exit()
     if return_traces:
         left_trace = traces[evaluations > 0]
         right_trace = traces[evaluations <= 0]
@@ -87,7 +92,7 @@ def split_with_formula(traces: np.ndarray, formula: Formula, return_traces=False
     if binary:
         threshold = find_best_binary_threshold(evaluations, labels, n=20)
         formula.boundary = -threshold
-        evaluations = formula.evaluate3(traces).min(axis=1)
+        evaluations = formula.evaluate(traces).min(axis=1)
     left_lab = labels[evaluations > 0]
     left_rob = evaluations[evaluations > 0]
     right_lab = labels[evaluations <= 0]
@@ -95,13 +100,13 @@ def split_with_formula(traces: np.ndarray, formula: Formula, return_traces=False
     return left_lab, left_rob, right_lab, right_rob
 
 
-def choose_formula(traces: np.ndarray, binary=False) -> Formula:
+def choose_formula(traces: np.ndarray, binary=False):
     best_entropy = np.inf
     best_formula = None
     values = traces[:, :-1].astype(float)
-    boundaries = np.linspace(values.min(), values.max(), num=3)
-    for boundary, end, operator in Formula.list_options(binary=binary, boundaries=boundaries):
-        formula = Formula.build_formula(boundary=boundary, end=end, operator=operator)
+    boundaries = np.linspace(values.min(), values.max(), num=5)
+    for boundary, end, operator in FormulaFactory.list_options(binary=binary, boundaries=boundaries):
+        formula = FormulaFactory.build_formula(boundary=boundary, end=end, operator=operator)
         left_lab, left_rob, right_lab, right_rob = split_with_formula(traces, formula, binary=binary)
         H_1 = stl_entropy(left_lab, left_rob, right_lab, right_rob)
         H_2 = entropy(left_lab, right_lab)
@@ -166,9 +171,9 @@ class TreeNode:
         if self.is_leaf():
             return prev_formula + " --> " + self.value + "\n"
         left_spec = prev_formula + (
-            self.formula.spec if verbose else self.formula.stl_spec
+            self.formula.spec
         )
-        right_spec = prev_formula + (self.formula.negate(verbose=verbose))
+        right_spec = prev_formula + (self.formula.negate())
         conj = " and " if verbose else " ∧ "
         if self.left and not self.left.is_leaf():
             left_spec = f"({left_spec}){conj}"
@@ -189,8 +194,8 @@ class TreeNode:
     def classify(self, trace) -> str:
         if self.value:
             return self.value
-        evaluation = self.formula.evaluate3(trace.reshape(1, -1), labels=False)[0]
-        rob = evaluation.mean() if config["USE_MEAN"] else evaluation.min()
+        evaluation = self.formula.evaluate(trace.reshape(1, -1), labels=False)[0]
+        rob = evaluation.min()
         next_node = self.left if rob > 0 else self.right
         return next_node.classify(trace)
 
@@ -234,7 +239,7 @@ class TreeNode:
             if self.value != trace[-1]:
                 rebuild_tree()
             return
-        rob = self.formula.evaluate(trace.reshape(1, -1), labels=True)
+        rob = self.formula.evaluate(trace.reshape(1, -1), labels=True).min()
         next_node = self.left if rob > 0 else self.right
         expected_label = next_node.value if next_node.is_leaf() else choose_majority(next_node.labels)
         if trace[-1] != expected_label: