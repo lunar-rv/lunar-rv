d743a92c3ecd25cdfa6a0d194e560ad79a69227b
Anonymous <anonymous@example.com>
2024-09-05 16:09:31 +0100

    Build: rv.py options

--- src/rv.py
+++ src/rv.py
@@ -3,30 +3,48 @@ from monitor import run_monitor
 import argparse
 from offline import *
 import json
+
+# Load configuration file
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
 
 print("Parsing specification...")
 
+# Argument parser setup
 argparser = argparse.ArgumentParser()
 argparser.add_argument("spec_file", help="The file containing the specification")
-argparser.add_argument("--test", help="The test to run")
+argparser.add_argument("--test", default=-1, help="The test to run")
+argparser.add_argument("--index", nargs='?', default=None, help="Sensor index to use (optional)")
+argparser.add_argument("--model", nargs='?', default=None, help="Model to use (optional)")
 args = argparser.parse_args()
 
 def main():
 
     p = Parser()
     p.parse(args.spec_file)
+
     if config["ONLINE"]:
         run_monitor(p)
     else:
+        if args.test == -1:
+            print("Error: no test specified.")
+            print("Offline mode is for tool evaluation only.")
+            print("In order to run the monitor online, please set the ONLINE flag to True in the config file.")
+            print("Otherwise, please specify a test to run, e.g. python src/rv.py example.spec --test 1")
+            exit()
         import sys
-        if len(sys.argv) == 3:
+        # Check if a test function exists
+        if len(sys.argv) >= 3:
             func = globals()[f"testing_{args.test}"]
-            func(p)
+            
+            # Handle test 4 with optional index and model arguments
+            if int(args.test) == 4:
+                func(p, s_index=int(args.index) if args.index else None, model_type=args.model)
+            else:
+                func(p)
         else:
+            # Default to running test 6 if no other test is specified
             testing_6(p)
-
 
 if __name__ == "__main__":
     main()
